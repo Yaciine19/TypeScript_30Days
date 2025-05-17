e1395b7f399e9cd892f3de3740304ef2fadaa26a
yacine <yacineddd32@gmail.com>
2025-05-17 11:36:58 +0100

    reduce function

--- Array cardio day 1/main.js
+++ Array cardio day 1/main.js
@@ -26,9 +26,16 @@ console.table(inventorsBornIn1500s);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventory first and last names
-
 const inventoriesFullName = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
 console.log(inventoriesFullName);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+console.table(inventors.sort((a, b) => a.year - b.year));
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
+const totalYears = inventors.reduce((total, inventor) => {
+    return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(totalYears);
